18bb59f845c6f1d2b8c88115de1475983d2d7d61
Domjan <109731064+coldgreen101@users.noreply.github.com>
2024-03-09 14:53:06 +0000

    Sat, Mar 9, 2024, 3:53 PM +01:00

--- src/components/Greeting.svelte
+++ src/components/Greeting.svelte
@@ -13,4 +13,14 @@
   });
 </script>
 
-<
+<dialog open>
+  <article>
+    <header>
+      <button aria-label="Close" rel="prev"></button>
+      <h3>
+        <strong>{greeting}! Thank You for visiting!</strong>
+      </h3>
+    </header>
+    <button on:click={randomMessage}>New Greeting</button>
+  </article>
+</dialog>
