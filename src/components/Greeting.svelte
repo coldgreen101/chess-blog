088b488773b20246079d5695673b14e09c6380e4
Domjan <109731064+coldgreen101@users.noreply.github.com>
2024-03-09 14:48:16 +0000

    Sat, Mar 9, 2024, 3:48 PM +01:00

--- src/components/Greeting.svelte
+++ src/components/Greeting.svelte
@@ -1,3 +1,16 @@
 <script>
-  import {onMount} from 'svelte';
+  import { onMount } from 'svelte';
+
+  let messages = [];
+  let greeting = '';
+
+  const randomMessage = () => {
+    greeting = messages[Math.floor(Math.random() * messages.length)];
+  }
+
+  onMount(() => {
+    randomMessage();
+  });
 </script>
+
+<
