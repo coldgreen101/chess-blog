9d4086ea78b55c4fc76317ff4e32cdbe8d785dbf
Domjan <109731064+coldgreen101@users.noreply.github.com>
2024-03-10 13:36:03 +0000

    Sun, Mar 10, 2024, 2:36 PM +01:00

--- src/components/Greeting.svelte
+++ src/components/Greeting.svelte
@@ -16,3 +16,13 @@
     greeting.set(randomMessage());
   }
 </script>
+
+<dialog open>
+  <article>
+    <header>
+      <button aria-label="Close" rel="prev"></button>
+      <h3><strong>{$greeting}! Thank you for visiting!</strong></h3>
+    </header>
+    <button on:click={setGreeting}> New Greeting</button>
+  </article>
+</dialog>
