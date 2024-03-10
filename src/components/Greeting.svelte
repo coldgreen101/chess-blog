bf601da8d770eacf0e1a3330b9880074b54ebb18
Domjan <109731064+coldgreen101@users.noreply.github.com>
2024-03-10 11:34:54 +0000

    Sun, Mar 10, 2024, 12:34 PM +01:00

--- src/components/Greeting.svelte
+++ src/components/Greeting.svelte
@@ -2,13 +2,11 @@
   export let messages;
 
   import { onMount } from 'svelte';
-  import { writable } from 'svelte/store';
-
-  const greeting = writable(messages[0]);
+  let greeting = messages[0];
 
   const randomMessage = () => {
     const randomIndex = Math.floor(Math.random() * messages.length);
-    greeting.set(messages[randomIndex]);
+    greeting = messages[randomIndex];
   };
 
   onMount(() => {
@@ -20,7 +18,7 @@
   <article>
     <header>
       <button aria-label="Close" rel="prev"></button>
-      <h3><strong>{$greeting}! Thank You for Visiting!</strong></h3>
+      <h3><strong>{greeting}! Thank You for Visiting!</strong></h3>
     </header>
     <button onClick={randomMessage}> New Greeting </button>
   </article>
