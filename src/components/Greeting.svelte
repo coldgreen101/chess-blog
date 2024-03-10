aa506bf800aaa8d04e445dbaf00df1201a65be7d
Domjan <109731064+coldgreen101@users.noreply.github.com>
2024-03-10 12:41:20 +0000

    Sun, Mar 10, 2024, 1:41 PM +01:00

--- src/components/Greeting.svelte
+++ src/components/Greeting.svelte
@@ -22,6 +22,6 @@
       <button aria-label="Close" rel="prev"></button>
       <h3><strong>{$greeting}! Thank You for Visiting!</strong></h3>
     </header>
-    <button on:click={randomMessage}> New Greeting </button>
+    <button onClick={randomMessage}> New Greeting </button>
   </article>
 </dialog>
