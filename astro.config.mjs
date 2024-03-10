ab22e611d5fc7c375dfed03ef889f544ff63e4b6
Domjan <109731064+coldgreen101@users.noreply.github.com>
2024-03-10 13:12:34 +0000

    Sun, Mar 10, 2024, 2:12 PM +01:00

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config';
 
-import svelte from "@astrojs/svelte";
+import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://gambit-chronicles.netlify.app/",
+  integrations: [preact()]
 });
