4b4fe13d51f8fea7498253724456c653f4d904ca
Domjan <109731064+coldgreen101@users.noreply.github.com>
2024-03-10 15:35:10 +0000

    Sun, Mar 10, 2024, 4:35 PM +01:00

--- src/components/Greeting.svelte
+++ src/components/Greeting.svelte
@@ -1,9 +1,11 @@
 <script>
+  // Import necessary functions from Svelte
   import { onMount } from 'svelte';
   import { writable } from 'svelte/store';
 
   export let messages;
 
+  // Retrieve a random message from the message array
   const randomMessage = () =>
     messages[Math.floor(Math.random() * messages.length)];
   const greeting = writable(messages[0]);
@@ -16,6 +18,7 @@
     greeting.set(randomMessage());
   }
 
+  // Function to close the dialog when called
   function closeDialog() {
     const dialog = document.querySelector('dialog');
     dialog.close();
@@ -25,11 +28,13 @@
 <dialog open>
   <article>
     <header>
+      <!-- Button to close the dialog -->
       <button aria-label="Close" rel="prev" on:click={closeDialog}></button>
       <h3>
         <strong>{$greeting}! Thank you for visiting!</strong>
       </h3>
     </header>
+    <!-- Button to set a new random greeting -->
     <button on:click={setGreeting}> New Greeting</button>
   </article>
 </dialog>
