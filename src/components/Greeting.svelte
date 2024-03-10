e6a478a9c9a394fb4c164d7ce53bf8c24ce97b37
Domjan <109731064+coldgreen101@users.noreply.github.com>
2024-03-10 11:09:21 +0000

    Sun, Mar 10, 2024, 12:09 PM +01:00

--- src/components/Greeting.svelte
+++ src/components/Greeting.svelte
@@ -1,1 +1,17 @@
+<script>
+  import { onMount } from 'svelte';
 
+  export let messages;
+
+  function getRandomIndex() {
+    return Math.floor(Math.random() * messages.length);
+  }
+
+  let randomIndex = getRandomIndex();
+  let greeting = messages[randomIndex];
+
+  function changeGreeting() {
+    randomIndex = getRandomIndex();
+    greeting = messages[randomIndex]
+  }
+</script>
