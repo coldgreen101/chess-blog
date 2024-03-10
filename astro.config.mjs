47b15e65b32f1e293080c84b84cddc6746777eda
Domjan <109731064+coldgreen101@users.noreply.github.com>
2024-03-10 13:16:51 +0000

    Sun, Mar 10, 2024, 2:16 PM +01:00

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import svelte from "@astrojs/svelte";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://gambit-chronicles.netlify.app/",
+  integrations: [svelte()]
 });
---
+++ src/components/Greeting.svelte
@@ -0,0 +1,32 @@
+<script>
+  import { onMount } from 'svelte';
+  import { writable } from 'svelte/store';
+
+  export let messages;
+
+  const randomMessage = () =>
+    messages[Math.floor(Math.random() * messages.length)];
+  const greeting = writable(messages[0]);
+
+  onMount(() => {
+    greeting.set(messages[0]);
+  });
+
+  function setGreeting() {
+    greeting.set(randomMessage());
+  }
+</script>
+
+<dialog open>
+  <article>
+    <header>
+      <button aria-label="Close" rel="prev"></button>
+      <p>
+        <strong>🗓️ Thank You for Registering!</strong>
+      </p>
+    </header>
+    <button>
+      New Greeting
+    </button>
+  </article>
+</dialog>
