6ce4b157ba3fd41958c2f40ca4160e8264bf4c50
Domjan <109731064+coldgreen101@users.noreply.github.com>
2024-03-10 11:26:27 +0000

    Sun, Mar 10, 2024, 12:26 PM +01:00

--- src/components/Greeting.svelte
+++ src/components/Greeting.svelte
@@ -1,21 +1,25 @@
 <script>
-  export default function Greeting({ messages }) {
-    const randomMessage = () =>
-      messages[Math.floor(Math.random() * messages.length)];
-    const [greeting, setGreeting] = messages[0];
+  export let messages;
 
-    return (
-      <dialog open>
-        <article>
-          <header>
-            <button aria-label="Close" rel="prev"></button>
-            <h3>{greeting}<strong>Thank You for Visiting!</strong></h3>
-          </header>
-          <button onClick={() => setGreeting(randomMessage())}>
-            New Greeting
-          </button>
-        </article>
-      </dialog>
-    );
-  }
+  import { onMount } from 'svelte';
+  let greeting = messages[0];
+
+  const randomMessage = () => {
+    const randomIndex = Math.floor(Math.random() * messages.length);
+    greeting = messages[randomIndex];
+  };
+
+  onMount(() => {
+    randomMessage();
+  });
 </script>
+
+<dialog open>
+  <article>
+    <header>
+      <button aria-label="Close" rel="prev"></button>
+      <h3>{greeting}<strong>Thank You for Visiting!</strong></h3>
+    </header>
+    <button onClick={randomMessage}> New Greeting </button>
+  </article>
+</dialog>
